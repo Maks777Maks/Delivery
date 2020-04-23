d7d7f9f2cf7bb265370d110badb5973516f80d50
Stepan <stepansmetanskyy@gmail.com>
2020-04-23 11:49:13 +0300

    create userProfile view

--- Delivery/ClientApp/src/App.js
+++ Delivery/ClientApp/src/App.js
@@ -8,6 +8,10 @@ const LoginPage = React.lazy(() => import("./views/defaultviews/LoginPage"));
 // Layouts
 const AdminLayout = React.lazy(() => import("./layouts/adminLayout/AdminLayout"));
 
+// ProfileTest
+const ProfileLayout = React.lazy(() => import("./views/ProfileManager/"));
+
+
   class App extends Component { 
 
     state = {
@@ -22,6 +26,7 @@ const AdminLayout = React.lazy(() => import("./layouts/adminLayout/AdminLayout")
           <Switch>
             <Route path="/admin" name="Admin" render={ props => <AdminLayout { ...props } /> } />
             <Route exact path="/login" name="Login" render={ props => <LoginPage { ...props } /> } />
+            <Route exact path="/profile" name="Profile" render={ props => <ProfileLayout { ...props } /> } />
           </Switch>
         </Suspense>
         </Router> 
