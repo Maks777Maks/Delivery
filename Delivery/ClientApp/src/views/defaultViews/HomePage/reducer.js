0b63e09ae3b6e7deaebb89cfecd5ea237e3b3c76
w1nyx <vad.com.z@gmail.com>
2020-05-14 10:55:45 +0300

    add localization and some fixes to HomePage

---
+++ Delivery/ClientApp/src/views/defaultViews/HomePage/reducer.js
@@ -0,0 +1,8 @@
+import HomePageService from "./HomePageService";
+import update from "../../../helpers/update";
+
+export const INFO_SUCCESS = "INFO_SUCCESS";
+
+const initialState = {
+    randomMenuPhoto: []
+  };
