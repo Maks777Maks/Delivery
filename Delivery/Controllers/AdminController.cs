6df6d58049740eaaf1268a022e24e81dd665ac4a
TimonUA <57059072+TimonUA@users.noreply.github.com>
2020-05-17 09:32:14 +0300

    just commit

--- Delivery/Controllers/AdminController.cs
+++ Delivery/Controllers/AdminController.cs
@@ -1,11 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Delivery.DAL.EFContext;
 using Delivery.ViewModels;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
